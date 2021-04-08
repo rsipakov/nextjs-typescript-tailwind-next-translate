6f54dee2ae2ff5cb79f391c34572177f0e66334f
Rostyslav Sipakov <ross.s@missioniz.com>
2021-04-08 19:22:03 -0400

    Update packages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const path = require('path');
 const nextTranslate = require('next-translate');
 
 module.exports = {
+    future: {
+        webpack5: true,
+    },
     ...nextTranslate(),
     sassOptions: {
         includePaths: [path.join(__dirname, './src/styles')]
