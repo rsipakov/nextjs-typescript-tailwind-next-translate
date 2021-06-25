c1524040009bc1791e77fc41104424f5080a207f
Rostyslav <ross.s@missioniz.com>
2021-06-25 03:13:38 -0400

    add layouts to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     },
     purge: {
         mode: 'all',
-        content: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}']
+        content: [
+            './src/components/**/*.{js,ts,jsx,tsx}',
+            './src/pages/**/*.{js,ts,jsx,tsx}',
+            './src/layouts/**/*.{js,ts,jsx,tsx}'
+        ]
     },
     theme: {
         extend: {
